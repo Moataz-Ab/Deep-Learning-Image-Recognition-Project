47175ad1964041a283f6bdd35ec77f49a68b8841
Yusuf Ayub <yusayub@hotmail.com>
2023-09-07 22:30:52 +0200

    added post endpoint and posted image with curl, the api responds as expected

--- recognition/api/fast.py
+++ recognition/api/fast.py
@@ -1,9 +1,9 @@
 ## model imports here
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from recognition.interface.predict import predict_image
 from recognition.helpers.load_model import load_model
-from recognition.helpers.load_image import load_image_tensor
+from recognition.helpers.load_image import load_image_tensor, load_image_from_file
 from recognition.interface.preprocessor import preprocess_image_tensor
 
 import os
@@ -22,6 +22,20 @@ app.add_middleware(
 @app.on_event("startup")
 async def startup_event():
     app.state.model = load_model(target="gcs") # load once
+
+@app.post("/predict")
+async def predict(file: UploadFile):
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        print(f"✔️ file received")
+        image_tensor = load_image_from_file(file.file)
+        image_processed = preprocess_image_tensor(image_tensor)
+        class_name = predict_image(image_processed=image_processed, model=app.state.model)
+        
+    return {
+        "predicted_class": f"{class_name}"
+    }
 
 @app.get("/predict")
 def predict():
