5d36a74cfd7abd0bd5df3f38aa89f13f8a3ae9fa
Yusuf Ayub <yusayub@hotmail.com>
2023-09-07 20:39:46 +0200

    fixed preprocessing image for prediction, prediction works, cycle api-predict finished

--- recognition/interface/predict.py
+++ recognition/interface/predict.py
@@ -1,10 +1,12 @@
-from recognition.interface.model import preprocess_input
-from recognition.helpers.load_model import load_model
-from recognition.interface.preprocessor import preprocess_image
 
-def predict(image, model):
-  
-  X_processed = preprocess_image(image)
-  y_pred = model.predict(X_processed)
-  print("\n✅ prediction done: ", y_pred, "\n")
-  return y_pred
+from recognition.helpers.load_model import load_model
+import numpy as np
+from recognition.interface.preprocessor import preprocess_image_tensor
+aircraft_classes = ['737', '747', '767', 'A340', 'CRJ', 'DC', 'DHC', 'E', 'MD']
+def predict_image(image_processed, model):
+  y_pred = model.predict(image_processed)
+  output = np.array(y_pred)
+  predicted_index = np.argmax(output)
+  predicted_class = aircraft_classes[predicted_index]
+  print("\n✅ prediction done: ", predicted_class, "\n")
+  return predicted_class
